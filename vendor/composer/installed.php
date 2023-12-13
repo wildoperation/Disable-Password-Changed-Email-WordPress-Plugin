9dd9756d209a3573ad1853abb6f41c723e16dd60
Tim Gieseking <timstl@gmail.com>
2023-12-13 15:10:39 -0600

    update composer.json to exclude files while archiving.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => 'ce83941b301d300b8500f098d7b593124d83981a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => false,
+        'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => 'ce83941b301d300b8500f098d7b593124d83981a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
